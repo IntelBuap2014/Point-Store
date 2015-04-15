1cd2823e8dee21bf8f4b01e40c27f66fdd56421e
bsanchezR <b.sanchez.rom@gmail.com>
2015-04-15 01:07:35 -0500

    Modificaciones socket

--- source/socket_fork.c
+++ source/socket_fork.c
@@ -19,7 +19,7 @@ void procesar (int sock)
 	n = read(sock,buffer,255);
 	if (n < 0)
 	{
-		perror("Error al leer del socket");
+		perror("Error al leer del socket\n");
 		exit(1);
 	}
 	//Se imprime el mensaje que envio el socket
@@ -28,24 +28,24 @@ void procesar (int sock)
 	n = write(sock,"Respuesta a socket",18);
 	if (n < 0)
 	{
-		perror("Error al escribir en el socket");
+		perror("Error al escribir en el socket\n");
 		exit(1);
 	}
 }
 
-int main(int argc, char *argv[])
+int main()
 {
 	struct sockaddr_in sockaddr, clientaddr;	//Estructuras para almacenar la info del socket
 	int s;						//Socket principal
 	int ns;						//Socket que se crea despues del accept()
 	int pid;					//Idetificador de procesos fork()
 	int len;					//Longitud de estrucutras
-	int portno = 5001;				//Puerto de comunicacion
+	int portno = 51717;				//Puerto de comunicacion
 	//Se abre el socket y se verifican errores
 	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		perror("Error al crear socket");
+		perror("Error al crear socket\n");
 	//Inicializar estructura del socket
-	memset(&sockaddr,'0',sizeof(sockaddr));
+	memset(&sockaddr,0,sizeof(sockaddr));
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_port = htons(portno);
 	sockaddr.sin_addr.s_addr = INADDR_ANY;
@@ -53,30 +53,33 @@ int main(int argc, char *argv[])
 	len=sizeof(sockaddr);
 	//Se liga al socket usando bind y se verifican errores
 	if (bind(s, (struct sockaddr *)&sockaddr, len) < 0)
-		perror("Error en el bind");
+		perror("Error en el bind\n");
 	//Señal para ignorar la terminacion de cada hijo
 	signal(SIGCHLD, SIG_IGN);
 	//Se coloca al socket en modo pasivo a la espera de conexiones y se verifican errores
 	if (listen(s,10) < 0)
-		perror("Eror en el listen");
+		perror("Eror en el listen\n");
 	//Ciclo infinito para aceptar conexiones
+	printf("Socket disponible\n");
 	while(1)
 	{
 		//Longitud de ls estructura		
-		len=sizeof(clientaddr);
+		len=sizeof(sockaddr);
 		//Se espera por conexiones, se bloquea hasta que ocurre alguna
-		if ((ns = accept(s,(struct sockaddr *)&clientaddr,&len)) < 0)
-			perror("Error en el accept");
+		if ((ns = accept(s,(struct sockaddr *)&sockaddr,&len)) < 0)
+			perror("Error en el accept\n");
 		//Se crea un proceso hijo por cada conexion entrante
 		//Se verifica por algun error en fork()
+		printf("Conexion establecida\n");
 		if((pid = fork()) < 0)
 		{ 
-			perror("Error en fork");
+			perror("Error en fork\n");
 			exit(1);
 		}
 		//El proceso hijo atiende la nuevo conexion
 		if(pid == 0)
 		{
+			printf("Atendiendo conexion\n");
 			//Se llama a la funcion que procesara los datos
 			procesar(ns);
 			exit(0);
