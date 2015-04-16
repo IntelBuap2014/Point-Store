233d95a262684c88a2f3eb58ad9e7b0b6c7d6a95
YNS-Ramirez <yeniseirape@gmail.com>
2015-04-16 01:16:42 -0500

    Agregando fuente para la conexion a servidor MySQL, consultando la tabla <<Usuario>>

--- src/conexionMySQL/conexionMySQL.c
+++ src/conexionMySQL/conexionMySQL.c
@@ -10,7 +10,7 @@ int main()
     char *server = "localhost"; /*direccion del servidor 127.0.0.1, localhost*/
     char *user = "root"; /*usuario permitido acceder a la base de datos */
     char *password = ""; /* contraseña para el usuario */
-    char *database = "prueba"; /*nombre de la base de datos a consultar */
+    char *database = "Point-Store"; /*nombre de la base de datos a consultar */
     conexion = mysql_init(NULL); /*inicializacion a nula la conexión */
 
     /* conectar a la base de datos */
@@ -21,16 +21,16 @@ int main()
     }
 
     /* enviar consulta SQL */
-    if (mysql_query(conexion, "select * from datos"))
+    if (mysql_query(conexion, "SELECT * FROM Usuario"))
     {
         fprintf(stderr, "%s\n", mysql_error(conexion));
         exit(1);
     }
 
     res = mysql_use_result(conexion);
-    printf("ID\tNombre\t\tedad\n");
+    printf("ID\tNombre\t\tNivel\t\tPassword\n");
     while ((row = mysql_fetch_row(res)) != NULL)
-        printf("%s\t%s\t%s \n", row[0],row[1],row[2]);
+        printf("%s\t%s\t%s\t%s \n", row[0],row[1],row[2],row[3]);
 
     /* se libera la variable res y se cierra la conexión */
     mysql_free_result(res);
