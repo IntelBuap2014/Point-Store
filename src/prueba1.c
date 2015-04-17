d6bab16f6d22d1cea27f28f04f1c13380f0a3d30
RozyP <rozydicemiau@gmail.com>
2015-04-17 12:39:03 -0500

    Conexion BD de usuario

--- src/prueba1.c
+++ src/prueba1.c
@@ -1,35 +1,93 @@
 #include <my_global.h>
 #include <mysql.h>
+#include "usuario.h"
+#include <string.h>
+const int MAX_BUF=1000;
+char* cad;
 
-int main(int argc, char **argv)
-{  
-  MYSQL *con = mysql_init(NULL);
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);        
+}
+/*Funcion para verificar el login de un usuario*/
+int login(MYSQL *con, ptrUsuario u){
+cad=malloc(MAX_BUF);
+sprintf(cad,"SELECT nombre,password from Usuario where nombre='%s'",getNombre(u));
+printf(cad);
+if (mysql_query(con, cad )) 
+  {
+     finish_with_error(con);
+  }
+  
+  MYSQL_RES *result = mysql_store_result(con);
+if (result == NULL) 
+  {
+     finish_with_error(con);
+  }
 
-  if (con == NULL) 
+  int num_fields = mysql_num_fields(result);
+  MYSQL_ROW row;
+   
+  while ((row = mysql_fetch_row(result))) 
+  { 
+      int i;
+	for(i = 0; i < num_fields; i++) 
+      { 
+          printf("%s ", row[i] ? row[i] : "NULL"); 
+      } 
+          printf("\n"); 
+  }
+ mysql_free_result(result);
+}
+
+/*Funcion para agregar un usuario a la base de datos*/
+int addNew(MYSQL *con, ptrUsuario  u){
+	cad=malloc(MAX_BUF);
+ 	sprintf(cad,"INSERT INTO Usuario VALUES('%d','%s','%s','%s') ",getId(u),getNombre(u),getNombre(u),getNombre(u));
+
+//FALTA verificar si ese usuario ya existe
+if (mysql_query(con, cad))
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      mysql_close(con);
+      exit(1);
+  }
+}
+int openConexion (MYSQL *con )
+{
+ if (con == NULL) 
   {
       fprintf(stderr, "%s\n", mysql_error(con));
       exit(1);
   }
 //Conexion a la base de datos 
-  if (mysql_real_connect(con, "localhost", "root", "123", 
-         "mydb", 0, NULL, 0) == NULL) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }  
+if (mysql_real_connect(con, "localhost", "root", "123",
+           "mydb", 0, NULL, 0) == NULL)
+            {
+                  fprintf(stderr, "%s\n", mysql_error(con));
+                         mysql_close(con);
+                               exit(1);
+                                 }
 
-  if (mysql_query(con, "INSERT INTO Usuario VALUES (1,'Beo','Norml','paw')")) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      
-
-
-      mysql_close(con);
-      exit(1);
-  }
-
-
-  mysql_close(con);
-  exit(0);
 }
+
+int closeConexion(MYSQL *con){
+    mysql_close(con);
+    exit(1);
+
+}
+int main(int argc, char **argv)
+{  
+  MYSQL *con = mysql_init(NULL);
+  openConexion(con);
+  ptrUsuario  u = constructZ("Nua", "pass");
+ // addNew(con,u);
+  login(con, u);
+  closeConexion(con);
+  exit(0);
+
+}
+
+
